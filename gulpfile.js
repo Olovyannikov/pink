2adb5e24de5aa34042a241254ea6689e4ba68c05
Olovyannikov <excaliburgyro@gmail.com>
2020-07-08 11:13:45 +0300

    Update concept
    Mobile first, optimization graphics, use gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,8 @@ var rename = require("gulp-rename");
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
 var jsmin = require('gulp-jsmin');
-var del = require("del"); 
+var del = require("del");
+var run = require("run-sequence");
 
 gulp.task("clean", function() {
   return del("build");
@@ -21,12 +22,12 @@ gulp.task("clean", function() {
 
 gulp.task("copy", function() {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
+    "source/fonts/**/*.{woff,woff2,eot,ttf,svg}",
     "source/img/**",
     "source/js/**",
     "source/*.html"
   ], {
-    base: "."
+    base: "source"
   })
     .pipe(gulp.dest("build"));
 });
@@ -43,35 +44,35 @@ gulp.task("style", function() {
         sort: false
       })
     ]))
-    .pipe(gulp.dest("build/source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("images", function() {
-  return gulp.src("build/source/img/**/*.{png,jpg,gif}")
+  return gulp.src("source/img/**/*.{png,jpg,gif}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true})
     ]))
-    .pipe(gulp.dest("build/source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("symbols", function() {
-  return gulp.src("build/source/img/icons/*.svg")
+  return gulp.src("source/img/icons/*.svg")
     .pipe(svgmin())
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename("symbols.svg"))
-    .pipe(gulp.dest("build/source/img"));
+    .pipe(gulp.dest("build/img/icons"));
 });
 
 gulp.task("html:copy", function() {
   return gulp.src("source/*.html")
-    .pipe(gulp.dest("build/source"));
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("html:update", ["html:copy"], function(done) {
@@ -80,19 +81,19 @@ gulp.task("html:update", ["html:copy"], function(done) {
 });
 
 gulp.task("js", function() {
-  gulp.src("source/js/reviewsSlider.js")
-    .pipe(gulp.dest("build/source/js"))
+  gulp.src("source/js/script.js")
+    .pipe(gulp.dest("build/js"))
     .pipe(jsmin())
     .pipe(rename("script.min.js"))
-    .pipe(gulp.dest("build/source/js"));
+    .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("js:copy", function() {
-  return gulp.src("source/js/reviewsSlider.js")
-    .pipe(gulp.dest("build/source/js"))
-    .pipe(jsmin())
-    .pipe(rename("script.min.js"))
-    .pipe(gulp.dest("build/source/js"));
+  return gulp.src("source/js/script.js")
+      .pipe(gulp.dest("build/js"))
+      .pipe(jsmin())
+      .pipe(rename("script.min.js"))
+      .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("js:update", ["js:copy"], function(done) {
@@ -102,7 +103,7 @@ gulp.task("js:update", ["js:copy"], function(done) {
 
 gulp.task("serve", function() {
   server.init({
-    server: "build/source",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -111,7 +112,7 @@ gulp.task("serve", function() {
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("source/*.html", ["html:update"]);
-  gulp.watch("source/js/reviewsSlider.js", ["js:update"]);
+  gulp.watch("source/js/script.js", ["js:update"]);
 });
 
 gulp.task ("build", function(fn) {
